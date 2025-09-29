845d11fda3dcdbbb53b096d7abd86f358c45bb77
Algoritma Turk <developer@algoritmaturk.net>
2025-09-29 22:31:30 +0300

    CQRS-Mediatr

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,30 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProdcutCommand(string Name,List<string> Category, string Description,string ImageFile, decimal Price)
-    :IRequest<CreateProductResult>;
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler:IRequestHandler<CreateProdcutCommand,CreateProductResult>
+internal class CreateProductCommandHandler:ICommandHandler<CreateProdcutCommand,CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProdcutCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProdcutCommand command, CancellationToken cancellationToken)
     {
-        //Business logic to create a product
+        //Create Product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Descriptiom = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        //save to database
+        
+        //return CreateProductResult result
+        return new CreateProductResult(Guid.NewGuid());
         throw new NotImplementedException();
     }
 }
